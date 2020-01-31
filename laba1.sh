22aaeab1afd6836b4ba83402cde16b328274eee0
sajeruk <goga.petrovich12@gmail.com>
2020-01-31 07:36:58 +0300

    Move projects on github

---
+++ laba1.sh
@@ -0,0 +1,14 @@
+cd $HOME
+ls
+mkdir Random_Dir
+cd Random_Dir
+touch Random_File
+echo "Я такой то такой то" >> Random_File
+echo "Мне стока то лет" >> Random_File
+echo "Я учусь там то там то" >> Random_File
+mkdir ~/Random_Dir2
+cp ~/Random_Dir/Random_File ~/Random_Dir2/Random_File
+mkdir ~/Random_Dir/Random_Dir3
+rmdir ~/Random_Dir/Random_Dir3
+mv ~/Random_Dir/Random_File ~/Random_Dir/Random_File_New
+
